a426e3b2630ef5ac4a3c9bcff49922c812c1fc55
jianglibo <jianglibo@gmail.com>
2014-06-13 14:21:08 +0800

    init

---
+++ src/main/java/com/m3958/apps/lucenesolr/App.java
@@ -0,0 +1,13 @@
+package com.m3958.apps.lucenesolr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
